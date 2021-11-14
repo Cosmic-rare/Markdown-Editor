5c1cbece266deb8f85c44a9d09205293e629ba2c
Cosmic-rare <ysolakey1116@gmail.com>
2021-11-14 10:16:42 -0800

    HTML Editorができちゃった

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='main'>
+    <div class='editor'>
+      <textarea @input='onChange' v-model='markdown'># Markdown</textarea>
+    </div>
+    <div class='preview' v-html='html'>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text: '',
+      markdown: '<h1>Hello!</h1>\na\n<p>Oh! This is HTML Editor!</p>\n<style>\nh1{font-size:100px;color:#1f1;margin:0px;}\n</style>',
+      html: ''
+    }
+  },
+  methods: {
+    onChange() {
+      this.html = this.markdown
+    }
+  },
+  mounted: function(){
+    this.html = this.markdown
   }
 }
 </script>
+
+<style>
+textarea {
+  width: 200px;
+  height: 300px;
+}
+
+.preview {
+  width: 200px;
+  height: 300px;
+  background-color: #999;
+}
+</style>
